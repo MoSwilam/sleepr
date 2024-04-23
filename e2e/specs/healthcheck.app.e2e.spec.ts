a03b0ecc04f28c53e2bdb6ed66c1a4a993a81503
MoSwilam <muswilam@gmail.com>
2024-04-23 19:05:46 +0100

    e2e

---
+++ e2e/specs/healthcheck.app.e2e.spec.ts
@@ -0,0 +1,36 @@
+import { ping } from 'tcp-ping';
+// check the health of the reservation app
+describe('HealthCheck', () => {
+  beforeEach(async () => {
+    // Simple retry mechanism or delay
+    await new Promise(resolve => setTimeout(resolve, 2000)); // wait 5 seconds before starting tests
+  });
+
+  test('Reservations', async () => {
+    const res = await fetch('http://reservations:3000')
+    expect(res.ok).toBeTruthy()
+  })
+
+  test('Auth', async () => {
+    const res = await fetch('http://auth:3001')
+    expect(res.ok).toBeTruthy()
+  })
+
+  test('Payments', (done) => {
+    ping({ address: 'pdayments', port: 3003}, (err) => {
+      if (err) {
+        fail(err);
+      }
+      done();
+    })
+  })
+
+  test('Notifications', (done) => {
+    ping({ address: 'notifications', port: 3004}, (err) => {
+      if (err) {
+        fail(err);
+      }
+      done();
+    })
+  })
+})
